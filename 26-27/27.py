1922809fe8be1f82f5491334c2e6f58dff266ab1
Barsuk <bogd4n.grom@yandex.ru>
2026-04-09 15:42:07 +0300

    Update 27.py

--- 26-27/27.py
+++ 26-27/27.py
@@ -30,3 +30,35 @@ r2=max(math.dist(c2,dot) for dot in a2)
 r3=max(math.dist(c3,dot) for dot in a3)
 q2=max(r1,r2,r3)
 print(int(q1*10000),int(q2*10000))
+
+
+
+
+
+
+
+import math
+t=open('27_A_25128.txt')
+a1,a2=[],[]
+for i in t:
+    x,y=map(float,i.replace(',','.').split())
+    (a1,a2)[x<12].append([x,y])
+def centr(k):
+    return sum(x[0] for x in k)/len(k)
+print(int(((centr(a1)+centr(a2))*10000)))
+def centr1(k):
+    return sum(x[1] for x in k)/len(k)
+print(int(((centr1(a1)+centr1(a2))*10000)))
+
+
+f=open('27_B_25128.txt')
+a1,a2,a3,a4=[],[],[],[]
+for i in f:
+    x,y=map(float,i.replace(',','.').split())
+    if x<10 and y<10: a1.append([x,y])
+    elif y<20 and 11<x<20: a2.append([x,y])
+    elif y<12 and 21<x<27: a3.append([x,y])
+    elif y<28 and 27<x<33: a4.append([x,y])
+q1=int((centr(a1)+centr(a2)+centr(a3)+centr(a4))*10000)
+q2=int((centr1(a1)+centr1(a2)+centr1(a3)+centr1(a4))*10000)
+print(q1,q2)
